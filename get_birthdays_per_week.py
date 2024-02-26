2761a29c63ba488827691f51867d9dcf4b0f1ed7
Volodymyr Kamyshanov <volodymyr.kamyshanov@nortal.com>
2024-02-26 19:46:37 +0200

    Add phone number book bot

--- get_birthdays_per_week.py
+++ get_birthdays_per_week.py
@@ -1,6 +1,17 @@
 from collections import defaultdict
 from datetime import datetime, timedelta
 
+users = [
+    {"name": "Bill Gates", "birthday": datetime(1955, 10, 28)},
+    {"name": "Jeff Bezos", "birthday": datetime(1964, 1, 12)},
+    {"name": "Tim Cook", "birthday": datetime(1960, 11, 1)},
+    {"name": "Mark Zuckerberg", "birthday": datetime(1984, 5, 14)},
+    {"name": "Volodymyr Kamyshanov", "birthday": datetime(1997, 3, 6)},
+    {"name": "Birthday_user", "birthday": datetime(2024, 3, 1)},
+    {"name": "Birthday_user_same_day", "birthday": datetime(2024, 3, 1)},
+    {"name": "Birthday_user_another_day", "birthday": datetime(2024, 2, 28)},
+    {"name": "Birthday_user_this_week_but_not_congratulate", "birthday": datetime(2024, 3, 2)},
+]
 
 def get_congratulation_day(today, birthday):
     birthday_this_year = birthday.replace(year=today.year)
